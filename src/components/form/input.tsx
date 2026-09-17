d7383105e7437029e1c3e6a05be57bdba69eab24
abdulaziz <abdulazizumaraliyev823@gmail.com>
2026-09-17 20:10:40 +0500

    fix S1-08

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -35,7 +35,7 @@ export function FormInput<IForm extends FieldValues>({
     wrapperClassName,
     className,
     type = "text",
-    hideError = true,
+    hideError = false,
     uppercase = false,
     ...props
 }: IProps<IForm> & React.InputHTMLAttributes<HTMLInputElement>) {
@@ -47,7 +47,10 @@ export function FormInput<IForm extends FieldValues>({
     const error = getNestedValue(errors, name)
 
     const reg = register(name, {
-        required: required ? `${label}ni kiriting` : false,
+        required:
+            required ?
+                `${label || props.placeholder || "Ushbu maydon"}ni kiriting`
+            :   false,
         ...(uppercase && {
             setValueAs: (value: string) => String(value)?.toUpperCase(),
         }),
