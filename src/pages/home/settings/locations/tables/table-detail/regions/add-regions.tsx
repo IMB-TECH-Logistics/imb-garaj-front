d9cfba4702f5c03cc44df38507e9db57918c9c88
abdulaziz <abdulazizumaraliyev823@gmail.com>
2026-09-17 20:47:57 +0500

    fix S1-10

--- src/pages/home/settings/locations/tables/table-detail/regions/add-regions.tsx
+++ src/pages/home/settings/locations/tables/table-detail/regions/add-regions.tsx
@@ -76,6 +76,7 @@ const AddRegionsModal = ({ country_id }: AddRegionsModalProps) => {
                     required
                     name="name"
                     label="Joylashuv nomi"
+                    maxLength={255}
                     methods={form}
                 />
 
